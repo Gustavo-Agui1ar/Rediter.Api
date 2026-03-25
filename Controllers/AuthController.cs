32d3bbdd5ba3b5b15d655cd84c6c3f5383e12cc2
GusAgui2005 <gusagui2005@gmail.com>
2026-03-25 19:13:51 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rediter.Api.Controllers
+{
+    [ApiController]
+    [Route("/Auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Rediter")]
+        public IActionResult RediterAuth()
+        {
+            return Ok("Rediter auth successful");
+        }
+
+        [HttpPost("Google")]
+        public IActionResult GoogleAuth()
+        {
+            return Ok("Google auth successful");
+        }
+
+        [HttpPut("Register")]
+        public IActionResult Register()
+        {
+            return Ok("User registered successfully");
+        }
+    }
+}
